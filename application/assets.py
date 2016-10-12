4048b0bc2de80e5588f03311a37ac08bdaf7c311
Colm Britton <colmjude@gmail.com>
2016-10-12 16:55:41 +0100

    move to using a factory model for the flask app

--- application/assets.py
+++ application/assets.py
@@ -6,12 +6,30 @@ from flask_assets import Bundle, Environment
 
 from lib.sass_filter import LibSass
 
+def static(*path):
+    return os.path.join(os.path.dirname(__file__), 'static', *path)
+
+libsass_output = LibSass(include_paths=[
+    static('css'),
+    static('sass'),
+    static('govuk_frontend_toolkit/stylesheets'),
+    static('govuk_elements/public/sass')])
+
 # GOV.UK assets
+# css_govuk = Bundle(
+#  'css/govuk.scss',
+#  filters='scss',
+#  output='gen/css/govuk.css',
+#  depends="**/*.scss"
+# )
+
 css_govuk = Bundle(
   'css/govuk.scss',
-  filters='scss',
-  output='gen/css/govuk.css',
-  depends="**/*.scss"
+  filters=(libsass_output,),
+    output='gen/css/govuk_elements.css',
+    depends=[
+        '/static/govuk_elements/public/sass/**/*.scss',
+        '/static/govuk_frontend_toolkit/stylesheets/**/*.scss']
 )
 
 assets = Environment()
--- manage.py
+++ manage.py
@@ -3,13 +3,14 @@
 from flask import Flask
 from flask_script import Manager
 
+from application.factory import create_app
 from lib.govuk_assets import ManageGovUkAssets
 
 app = Flask(__name__)
 
-manager = Manager(app)
+manager = Manager(create_app)
 manager.add_command('install_all_govuk_assets', ManageGovUkAssets())
 
 if __name__ == "__main__":
     manager.run()
-    
+
